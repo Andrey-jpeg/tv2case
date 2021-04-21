dc3250f11504520c50d02db227d75a50acae4473
Casper <casperfj2011@gmail.com>
2021-04-21 13:21:00 +0200

    Added constructor for Admin class

--- src/main/java/sdu/sem2/se17/domain/auth/Admin.java
+++ src/main/java/sdu/sem2/se17/domain/auth/Admin.java
@@ -1,4 +1,7 @@
 package sdu.sem2.se17.domain.auth;
 
 public class Admin extends User {
+    public Admin(String username, String password, String email) {
+        super(username, password, email);
+    }
 }
