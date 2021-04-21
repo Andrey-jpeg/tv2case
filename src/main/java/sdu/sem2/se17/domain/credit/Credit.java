df656da7310bc6fceb04f8f96fce50293495ab07
Casper <casperfj2011@gmail.com>
2021-04-21 12:43:30 +0200

    Removed id from class

--- src/main/java/sdu/sem2/se17/domain/credit/Credit.java
+++ src/main/java/sdu/sem2/se17/domain/credit/Credit.java
@@ -2,21 +2,13 @@ package sdu.sem2.se17.domain.credit;
 
 /* Casper Fenger Jensen */
 public class Credit {
-    private long id;
+
     private Participant participant;
     private Role role;
 
     Credit(Participant participant, Role role){
         this.participant = participant;
         this.role = role;
-    }
-
-    public long getId() {
-        return id;
-    }
-
-    public void setId(long id) {
-        this.id = id;
     }
 
     public Participant getParticipant() {
