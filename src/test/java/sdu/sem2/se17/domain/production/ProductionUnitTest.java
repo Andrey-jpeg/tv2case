8f8dba7d96184a3760ddb4daebfb1a278aa62cef
Hampus <16954508+xdHampus@users.noreply.github.com>
2021-04-21 13:30:24 +0200

    Preliminary setup of tests
    Added those I thought were of relevance.

---
+++ src/test/java/sdu/sem2/se17/domain/production/ProductionUnitTest.java
@@ -0,0 +1,34 @@
+package sdu.sem2.se17.domain.production;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ProductionUnitTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    void createCredit() {
+    }
+
+    @Test
+    void testCreateCredit() {
+    }
+
+    @Test
+    void getCredits() {
+    }
+
+    @Test
+    void removeCredit() {
+    }
+}
