11d0c555b6aab1f654b45e7ddf3c0b3fe8d1696d
Hampus <16954508+xdHampus@users.noreply.github.com>
2021-04-26 14:40:43 +0200

    Implement login method in LoginController

--- src/main/java/sdu/sem2/se17/presentation/cms/LoginController.java
+++ src/main/java/sdu/sem2/se17/presentation/cms/LoginController.java
@@ -1,9 +1,14 @@
 package sdu.sem2.se17.presentation.cms;
+import javafx.application.Platform;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
+import javafx.scene.control.ButtonType;
 import javafx.scene.control.TextField;
 import sdu.sem2.se17.domain.CreditManagementController;
+
+import java.io.IOException;
 
 public class LoginController extends Controller {
 
@@ -22,7 +27,36 @@ public class LoginController extends Controller {
 
     @FXML
     public void login(ActionEvent event) {
-        System.out.println(this.creditManagementController.login(usernameInput.getText(), passwordInput.getText()));
+
+        var username = usernameInput.getText();
+        var password = passwordInput.getText();
+        if(!username.isBlank()  && !password.isEmpty()){
+            var loginResult = this.creditManagementController.login(usernameInput.getText(), passwordInput.getText());
+
+            if(loginResult){
+                displayChooseProduction();
+            }
+            else {
+                Alert alert = new Alert(Alert.AlertType.ERROR, "Login credentials were incorrect.", ButtonType.OK);
+                alert.showAndWait();
+            }
+        }
+
+        System.out.println();
     }
+
+    private void displayChooseProduction(){
+            try {
+                loginButton.getScene().setRoot(
+                        MainFX.loadFXML(
+                                "ChooseProduction",
+                                new ChooseProductionController(
+                                        creditManagementController)
+                        ));
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+    }
+
 }
 
