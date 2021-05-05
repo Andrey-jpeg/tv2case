092a4b75d2073e12761468181e86f1b1d09124a0
Hampus <16954508+xdHampus@users.noreply.github.com>
2021-05-05 15:54:32 +0200

    wtff

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -5,12 +5,15 @@ module sdu.sem2.SE17 {
     requires gson;
     requires java.sql;
     requires org.postgresql.jdbc;
+    requires java.naming;
 
     opens sdu.sem2.se17.presentation.consumersystem;
     opens sdu.sem2.se17.domain;
     opens sdu.sem2.se17.domain.auth;
     opens sdu.sem2.se17.domain.credit;
     opens sdu.sem2.se17.domain.production;
+    opens sdu.sem2.se17.persistence.data;
+    opens sdu.sem2.se17.persistence.db;
 
     exports sdu.sem2.se17.presentation.cms;
     exports sdu.sem2.se17.presentation.consumersystem;
@@ -18,4 +21,6 @@ module sdu.sem2.SE17 {
     exports sdu.sem2.se17.domain.auth;
     exports sdu.sem2.se17.domain.credit;
     exports sdu.sem2.se17.domain.production;
+    exports sdu.sem2.se17.persistence.data;
+    exports sdu.sem2.se17.persistence.db;
 }
